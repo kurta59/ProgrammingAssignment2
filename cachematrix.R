13f2fc45fea36d23c8e2668f2098dce53ff10edf
kurt <kurt@cyberanderson.com>
2017-03-12 14:23:20 -0500

    RProg Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## kca
+## The inversion of a matrix is costly and there is benefit to
+## caching the inverse of matices rather than repeat running of the 
+## computation
 
-## Write a short comment describing this function
+## This function creates a special "matix" object that can
+## cache it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+        
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the "matrix" created 
+## in function makeCacheMatrix.  If the inverse has been 
+## calculated and is unchanged then the inverse is retrieved 
+## from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
